3d87a40d709dcafd7dd3c103ff086a9472b62d6e
ParLelya <parlelya@vk.com>
2023-03-24 19:00:31 +0300

    Fixed header styles

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,50 +5,36 @@ import { Link } from 'react-router-dom';
 const Navigation = () => {
 	return (
 		<header className='header'>
-			<div className='row'>
-				{/* <nav className="nav">
-					<div className='nav-wrapper'>
-						<a href="/" className="brand-logo">
-							<img src='./logo.svg' alt='logo' />
-						</a>
-						<ul id="nav-mobile" className="hide-on-med-and-down">
-							<li>
-								<Link to="/">
-									<span>Главная</span>
-								</Link>
-							</li>
-							<li>
-								<Link to="/">
-									<span>Телеканалы</span>
-								</Link>
-							</li>
-							<li>
-								<Link to="/">
-									<span>Фильмы</span>
-								</Link>
-							</li>
-							<li>
-								<Link to="/">
-									<span>Сериалы</span>
-								</Link>
-							</li>
-							<li>
-								<a href="#!">
-									<i className="material-icons">settings_accessibility</i>Доступная среда
-								</a>
-							</li>
-						</ul>
-						
-					</div>
-				</nav> */}
-				<div className='col s12'>
-				<SearchBar />
-				<button className='login'>
-					<i className="material-icons">account_circle</i>
-					<label>Войти</label>
-				</button>
+			<img src='./logo.svg' className='brand-logo' alt='logo' />
+				<nav className="nav">
+					<Link to="/">
+						Главная
+					</Link>
+
+					<Link to="/">
+						Телеканалы
+					</Link>
+
+					<Link to="/">
+						Фильмы
+					</Link>
+
+					<Link to="/">
+						Сериалы
+					</Link>
+
+					<a href="#!">
+						<i className="material-icons">settings_accessibility</i>
+					</a>
+
+				</nav>
+				<div className='ui-form'>
+					<SearchBar />
+					<button className='login'>
+						<i className="material-icons" style={{fontSize: '20px'}}>account_circle</i>
+						<label>Войти</label>
+					</button>
 				</div>
-			</div>
 		</header>
 	)
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const SearchBar = () => {
 	const [value, setValue] = useState('')
@@ -6,13 +6,11 @@ const SearchBar = () => {
 
 	const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setValue(event.target.value)
-
 	}
 
-	const onClearClick = () => {
-		setValue('')
-		inputRef.current?.focus()
-	}
+	// useEffect(() => {
+	// 	inputRef.current?.focus()
+	// }, [])
 
 	return (
 		<div className='search'>
@@ -20,25 +18,16 @@ const SearchBar = () => {
 				xmlns="http://www.w3.org/2000/svg"
 				height="48"
 				viewBox="0 96 960 960"
+				fill='#849ab2'
 			>
 				<path d="M796 935 533 672q-30 26-69.959 40.5T378 727q-108.162 0-183.081-75Q120 577 120 471t75-181q75-75 181.5-75t181 75Q632 365 632 471.15 632 514 618 554q-14 40-42 75l264 262-44 44ZM377 667q81.25 0 138.125-57.5T572 471q0-81-56.875-138.5T377 275q-82.083 0-139.542 57.5Q180 390 180 471t57.458 138.5Q294.917 667 377 667Z" />
 			</svg>
 			<input
 				ref={inputRef}
 				value={value}
-				placeholder="Найти"
+				placeholder="Поиск"
 				onChange={onChangeInput}
 			/>
-			{value && (
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					height="48"
-					viewBox="0 96 960 960"
-					className="close"
-					onClick={onClearClick}
-				>
-					<path d="m249 849-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" />
-				</svg>)}
 		</div>
 	)
 }
