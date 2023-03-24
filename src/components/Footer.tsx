b22347a2581a0bd5fd6c6a73984094203334ea6d
ParLelya <parlelya@vk.com>
2023-03-24 18:06:30 +0300

    Working on styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Footer = () => {
 	return (
-		<div>Footer</div>
+		<footer className='page-footer'>
+			<div className="container">
+				<div className="row">
+					<div className="col l4 offset-l2 s12">
+						<h5>Разделы</h5>
+						<ul>
+							<li><a href="#!">Главная</a></li>
+							<li><a href="#!">Телеканалы</a></li>
+							<li><a href="#!">Фильмы</a></li>
+							<li><a href="#!">Сериалы</a></li>
+						</ul>
+					</div>
+					<div className="col l4 offset-l2 s12">
+						<h5>Помощь</h5>
+						<ul>
+							<li><a href="#!">Справка</a></li>
+							<li><a href="#!">Обратная связь</a></li>
+							<li><a href="#!">Пользовательское соглашение</a></li>
+							<li><a href="#!">Корпоративный сайт</a></li>
+						</ul>
+					</div>
+					<img className="col l4 offset-l2 s12" alt='Мобильное приложение' src='./qr.svg' />
+				</div>
+			</div>
+		</footer>
 	)
 }
 
