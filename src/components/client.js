a6f07e1ebd2967e0ff04d5ba51d4f7bc557645cb
ParLelya <parlelya@vk.com>
2023-03-26 18:31:52 +0300

    Worked with media player

---
+++ src/components/client.js
@@ -0,0 +1,114 @@
+import React from "react";
+
+var pc = null;
+
+class PureJsModule extends React.Component {
+
+  negotiate() {
+    pc.addTransceiver("video", { direction: "recvonly" });
+    pc.addTransceiver("audio", { direction: "recvonly" });
+    return pc
+      .createOffer()
+      .then(function (offer) {
+        return pc.setLocalDescription(offer);
+      })
+      .then(function () {
+        // wait for ICE gathering to complete
+        return new Promise(function (resolve) {
+          if (pc.iceGatheringState === "complete") {
+            resolve();
+          } else {
+            function checkState() {
+              if (pc.iceGatheringState === "complete") {
+                pc.removeEventListener("icegatheringstatechange", checkState);
+                resolve();
+              }
+            }
+            pc.addEventListener("icegatheringstatechange", checkState);
+          }
+        });
+      })
+      .then(function () {
+        var offer = pc.localDescription;
+        return fetch("/offer", {
+          body: JSON.stringify({
+            sdp: offer.sdp,
+            type: offer.type,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+        });
+      })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (answer) {
+        return pc.setRemoteDescription(answer);
+      })
+      .catch(function (e) {
+        alert(e);
+      });
+  }
+
+  test() {
+    const contrast = document.getElementById("contrast").value
+      ? document.getElementById("contrast").value
+      : 1;
+    const brightness = document.getElementById("brightness").value
+      ? document.getElementById("brightness").value
+      : 0;
+    const saturation = document.getElementById("saturation").value
+      ? document.getElementById("saturation").value
+      : 0;
+
+    fetch("/settings", {
+      body: JSON.stringify({
+        brightness: parseInt(brightness),
+        contrast: parseInt(contrast),
+        saturation: parseInt(saturation),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    }).then((r) => console.log(r.status));
+  }
+
+  start() {
+    var config = {
+      sdpSemantics: "unified-plan",
+    };
+
+    if (document.getElementById("use-stun").checked) {
+      config.iceServers = [{ urls: ["stun:stun.l.google.com:19302"] }];
+    }
+
+    pc = new RTCPeerConnection(config);
+
+    // connect audio / video
+    pc.addEventListener("track", function (evt) {
+      if (evt.track.kind === "video") {
+        document.getElementById("video").srcObject = evt.streams[0];
+      } else {
+        document.getElementById("audio").srcObject = evt.streams[0];
+      }
+    });
+
+    document.getElementById("start").style.display = "none";
+    this.negotiate();
+    document.getElementById("stop").style.display = "inline-block";
+  }
+
+  stop() {
+    document.getElementById("stop").style.display = "none";
+    document.getElementById("start").style.display = "inline-block";
+    // close peer connection
+    setTimeout(function () {
+      pc.close();
+    }, 500);
+  }
+};
+
+export default PureJsModule
--- src/pages/VideoPlayer.tsx
+++ src/pages/VideoPlayer.tsx
@@ -1,58 +1,115 @@
-import React from 'react'
+import React, { useRef, useMemo } from 'react'
 import { useAppDispatch, useAppSelector } from '../hooks/hooks'
-import { RootState } from '../store/store'
 import {
-	setLightnessValue,
+	setBrightnessValue,
 	setContrastValue,
 	setSharpnessValue,
 	setSaturationValue,
 } from '../slices/colorSlice'
+import { RootState } from '../store/store'
+import PureJsModule from '../components/client'
 
 const VideoPlayer: React.FC = () => {
-
+	
+	const media = useMemo(() => new (PureJsModule as any)(), [])
+	
 	const dispatch = useAppDispatch()
-	const { lightnessValue, contrastValue, sharpnessValue, saturationValue } = useAppSelector((state: RootState) => state.color)
+	const { brightnessValue, contrastValue, sharpnessValue, saturationValue } =
+		useAppSelector((state: RootState) => state.color)
 
-	const setLightness = (event: React.ChangeEvent<HTMLInputElement>): void => {
-		console.log(event.target.value);
-		dispatch(setLightnessValue(Number(event.target.value)))
+	const useStunRef = useRef<HTMLInputElement | null>(null)
+	const stopRef = useRef<HTMLButtonElement | null>(null)
+	const startRef = useRef<HTMLButtonElement | null>(null)
+
+	const setBrightness = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		dispatch(setBrightnessValue(Number(event.target.value)))
 	}
 
 	const setContrast = (event: React.ChangeEvent<HTMLInputElement>): void => {
-		console.log(event.target.value);
 		dispatch(setContrastValue(Number(event.target.value)))
 	}
 
 	const setSharpness = (event: React.ChangeEvent<HTMLInputElement>): void => {
-		console.log(event.target.value);
 		dispatch(setSharpnessValue(Number(event.target.value)))
 	}
 
 	const setSaturation = (event: React.ChangeEvent<HTMLInputElement>): void => {
-		console.log(event.target.value);
 		dispatch(setSaturationValue(Number(event.target.value)))
 	}
 
 	const handleSubmit = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
 		event.preventDefault()
-		localStorage.setItem('colorPreset', String([lightnessValue, contrastValue, sharpnessValue, saturationValue]))
+		localStorage.setItem('brightnessPreset', String(brightnessValue))
+		localStorage.setItem('contrastPreset', String(contrastValue))
+		localStorage.setItem('sharpnessPreset', String(sharpnessValue))
+		localStorage.setItem('saturationPreset', String(saturationValue))
 	}
 
 	return (
 		<div className='video-player'>
-			<video controls muted width="100%">
-				<source src="http://localhost:3000/video" type="video/mp4" />
-			</video>
+			<div className="option">
+				<input
+					id="use-stun"
+					type="checkbox"
+					checked
+					disabled
+					ref={useStunRef}
+				/>
+				<label htmlFor="use-stun">Use STUN server</label>
+			</div>
+			<div style={{ display: 'flex' }}>
+				<button
+					ref={startRef}
+					id="start"
+					onClick={() => media.start()}
+				>Старт</button>
+				<button
+					ref={stopRef}
+					id="stop"
+					style={{ display: 'none' }}
+					onClick={() => media.stop()}
+				>Стоп</button>
+				<div style={{ display: 'flex', flexDirection: 'column' }}>
+					<label htmlFor="contrast">контрастность</label>
+					<input
+						id="contrast"
+						placeholder="contrast"
+						type="number"
+					/>
+					<label htmlFor="brightness">яркость</label>
+					<input
+						id="brightness"
+						placeholder="brightness"
+						type="number"
+					/>
+					<label htmlFor="saturation">насыщенность</label>
+					<input
+						id="saturation"
+						placeholder="saturation"
+						type="number"
+					/>
+				</div>
+				<button
+					id="test"
+					onClick={() => media.test()}
+				>Тест</button>
+			</div>
+			<div id="media">
+				<audio id="audio" autoPlay={true}></audio>
+				<video id="video" autoPlay={true} playsInline={true}>
+				</video>
+			</div>
+
 			<form>
 				<p className="range-field">
 					<label>
 						<span>Настройка яркости</span>
 						<input
 							type="range"
-							id="lightness"
+							id="brightness"
 							min="0"
 							max="100"
-							onChange={setLightness}
+							onChange={setBrightness}
 						/>
 					</label>
 				</p>
@@ -77,6 +134,7 @@ const VideoPlayer: React.FC = () => {
 							min="0"
 							max="100"
 							onChange={setSharpness}
+							disabled
 						/>
 					</label>
 				</p>
