4381300ab11915784a764f2ff9e1811f7eb06fdc
ParLelya <parlelya@vk.com>
2023-03-26 12:30:11 +0300

    Small fixes

--- src/pages/Accessibility.tsx
+++ src/pages/Accessibility.tsx
@@ -1,51 +1,25 @@
 import React from 'react'
 import { useAppDispatch, useAppSelector } from '../hooks/hooks'
 import { RootState } from '../store/store'
-import {
-	setLightnessValue,
-	setContrastValue,
-	setSharpnessValue,
-	setSaturationValue,
-	setGrayScaleValue
-} from '../slices/colorSlice'
+import { setGrayScaleValue } from '../slices/colorSlice'
 
 const Accessibility: React.FC = () => {
 
 	const dispatch = useAppDispatch()
-	const { lightnessValue, contrastValue, sharpnessValue, saturationValue, grayScaleValue } = useAppSelector((state: RootState) => state.color)
+	const { grayScaleValue } = useAppSelector((state: RootState) => state.color)
 
 	const switchGreyShades = () => {
 		dispatch(setGrayScaleValue(100))
 	}
 
-	const setLightness = (event: React.ChangeEvent<HTMLInputElement>): void => {
-		console.log(event.target.value);	
-		dispatch(setLightnessValue(Number(event.target.value)))
-	}
-
-	const setContrast = (event: React.ChangeEvent<HTMLInputElement>): void => {
-		console.log(event.target.value);
-		dispatch(setContrastValue(Number(event.target.value)))
-	}
-
-	const setSharpness = (event: React.ChangeEvent<HTMLInputElement>): void => {
-		console.log(event.target.value);
-		dispatch(setSharpnessValue(Number(event.target.value)))
-	}
-
-	const setSaturation = (event: React.ChangeEvent<HTMLInputElement>): void => {
-		console.log(event.target.value);
-		dispatch(setSaturationValue(Number(event.target.value)))
-	}
-
 	const handleSubmit = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
 		event.preventDefault()
-		localStorage.setItem('colorPreset', String([lightnessValue, contrastValue, sharpnessValue, saturationValue, grayScaleValue]))
+		localStorage.setItem('accessibilityPreset', String(grayScaleValue))
 	}
 
 	return (
 		<form className='accessibility'>
-			<legend>Выберете цветовой режим или настройте самостоятельно</legend>
+			<legend>Выберете цветовой режим</legend>
 			<p>
 				<label>
 					<input type="checkbox" disabled />
@@ -74,58 +48,6 @@ const Accessibility: React.FC = () => {
 					<option>Дейтеранопия</option> {/*нет зеленого*/}
 					<option>Тританопия</option> {/*нет синего*/}
 				</select>
-			</p>
-			<p className="range-field">
-				<label>
-					<span>Настройка яркости</span>
-					<input
-						type="range"
-						id="lightness"
-						min="0"
-						max="100"
-						value={lightnessValue}
-						onChange={setLightness}
-					/>
-				</label>
-			</p>
-			<p className="range-field">
-				<label>
-					<span>Настройка контрастности</span>
-					<input
-						type="range"
-						id="contrast"
-						min="0"
-						max="200"
-						value={contrastValue}
-						onChange={setContrast}
-					/>
-				</label>
-			</p>
-			<p className="range-field">
-				<label>
-					<span>Настройка резкости</span>
-					<input
-						type="range"
-						id="sharpness"
-						min="0"
-						max="100"
-						value={sharpnessValue}
-						onChange={setSharpness}
-					/>
-				</label>
-			</p>
-			<p className="range-field">
-				<label>
-					<span>Настройка насыщенности</span>
-					<input
-						type="range"
-						id="saturation"
-						min="0"
-						max="200"
-						value={saturationValue}
-						onChange={setSaturation}
-					/>
-				</label>
 			</p>
 			<button type='submit' onClick={handleSubmit}>
 				Применить настройки
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
@@ -10,32 +11,43 @@ const Home = () => {
 					<a href='!'>Смотреть все <i className="large material-icons" style={{ fontSize: '1rem' }}>chevron_right</i></a>
 				</div>
 				<div className='posters-images'>
-					<img
-						src='./card1.svg'
-						alt='new films and series'
-					/>
-					<img
-						src='./card2.svg'
-						alt='new films and series'
-					/>
-					<img
-						src='./card3.svg'
-						alt='new films and series'
-					/>
-					<img
-						src='./card4.svg'
-						alt='new films and series'
-					/>
-					<img
-						src='./card5.svg'
-						alt='new films and series'
-						className='hide-on-med-and-down'
-					/>
-					<img
-						src='./card6.svg'
-						alt='new films and series'
-						className='hide-on-med-and-down'
-					/>
+				<Link to="/mediaplayer">
+						<img
+							src='./card1.svg'
+							alt='new films and series'
+						/>
+					</Link>
+					<Link to="/mediaplayer">
+						<img
+							src='./card2.svg'
+							alt='new films and series'
+						/>
+					</Link>
+					<Link to="/mediaplayer">
+						<img
+							src='./card3.svg'
+							alt='new films and series'
+						/>
+					</Link>
+
+					<Link to="/mediaplayer">
+						<img
+							src='./card4.svg'
+							alt='new films and series'
+						/>
+					</Link>
+					<Link to="/mediaplayer" className='hide-on-med-and-down'>
+						<img
+							src='./card5.svg'
+							alt='new films and series'
+						/>
+					</Link>
+					<Link to="/mediaplayer" className='hide-on-med-and-down'>
+						<img
+							src='./card6.svg'
+							alt='new films and series'
+						/>
+					</Link>
 				</div>
 			</div>
 
@@ -47,7 +59,14 @@ const Home = () => {
 			<img
 				src='./tv.svg'
 				alt='free tv channels'
-				style={{ marginTop: '2rem', width: '80%' }}
+				style={{ margin: '2rem auto', width: '80%' }}
+				className='hide-on-med-and-down'
+			/>
+			<img
+				src='./tv-mob.svg'
+				alt='free tv channels'
+				style={{ margin: '2rem auto', width: '80%'}}
+				className='show-on-medium-and-down'
 			/>
 		</div>
 	)
